206886fa615bba2c82dd672887c6a3895ed96b72
salveta <salveta4_@hotmail.com>
2014-07-08 16:00:51 +0200

    Fix three errors and firs refact

--- CodeBreaker.rb
+++ CodeBreaker.rb
@@ -1,7 +1,22 @@
 class CodeBreaker
 
+CODE = "RANI"
+
 	def self.check(value)
-		
+    compare_values(value)
 	end
 
+private
+
+	def self.compare_values(value)
+    answer=""
+    $i=0
+		while $i < 4 do
+      if value[$i].chr == CODE[$i].chr 
+        answer+="x" 
+      end
+        $i+=1
+    end
+    return answer
+  end
 end
--- spec/CodeBreaker_spec.rb
+++ spec/CodeBreaker_spec.rb
@@ -5,5 +5,12 @@ describe " CodeBreaker " do
 		expect(CodeBreaker.check("YMYI")).to eq "x"
 	end
 
+	it "identifies a color in two correct position" do
+		expect(CodeBreaker.check("RMVI")).to eq "xx"
+	end
+
+	it "identifies a color in three correct position" do
+		expect(CodeBreaker.check("RARI")).to eq "xxx"
+	end
 
 end
